68abf5758af44b126562b33c9cec00c265595287
manishparihar3618 <manishparihar3618@gmail.com>
2026-06-05 21:59:40 +0530

    Explored dataset 6

---
+++ notebooks/11_ds6_dataset_exploration.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df1 = pd.read_csv("data/DS6_historical_failures_2000.csv")
+
+print("Shape of the dataset:", df1.shape)
+print("\nColumns in the dataset:", df1.columns)
+print("\nFirst 5 rows of the dataset:", df1.head())
+print("\nData types of each column:\n", df1.dtypes)
+print("\nMissing values in each column:\n", df1.isnull().sum())
+print("\nStatistical summary of the dataset:\n", df1.describe())
+
